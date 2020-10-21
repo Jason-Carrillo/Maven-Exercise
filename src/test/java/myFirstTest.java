286fbca0f79ac3f1850079eb50aad7af083816ba
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-21 08:43:51 -0600

    added testArrays equals

--- src/test/java/myFirstTest.java
+++ src/test/java/myFirstTest.java
@@ -1,4 +1,8 @@
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class myFirstTest {
@@ -12,6 +16,25 @@ public class myFirstTest {
         assertNotEquals(a, b.toLowerCase());
     }
 
+    @Test
+    public void testIfListsTheSame(){
+        List<String> a = new ArrayList<>();
+        List<String> b = new ArrayList<>();
+
+        assertNotSame(a ,b);
+    }
+
+    @Test
+    public void testIfArraysAreEqual(){
+        int[] a = {1, 2, 3};
+        int[] b = new int[3];
+        b[0] = 1;
+        b[1] = 2;
+        b[2] = 3;
+
+        assertArrayEquals(a, b);
+
+    }
 
 
 }
