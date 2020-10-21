789cbf0608d30fb47b934317207ccb88f86e1d1d
Jason Carrillo <jason.carrillo182@gmail.com>
2020-10-21 08:36:46 -0600

    added testString method

--- src/main/java/myTest.java
+++ src/main/java/myTest.java
@@ -1,3 +1,23 @@
-public class myTest {
+import org.junit.Before;
 
-}
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+//public class myTest {
+//    private ArrayList<Device> devices;
+//
+//    @Before
+//    public void setUp(){
+//        this.devices = new ArrayList<Device>();
+//        // Run a DB backup
+//        // Clear your files
+//        this.devices.add(new Device());
+//        this.devices.add(new Device("Pixel 5"));
+//        this.devices.add(null);
+//    }
+//
+//
+//
+//}
---
+++ src/test/java/myFirstTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class myFirstTest {
+
+    @Test
+    public void testStrings() {
+        String a = "Codeup";
+        String b = "Codeup";
+
+        assertEquals(a, b);
+    }
+
+
+
+}
